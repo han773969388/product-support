97c909301e8940053e0d0842e3cf8bb635fc8770
gaohan <gaohan@ebay.com>
2025-08-14 13:34:02 +0800

    新增Product Support

---
+++ src/schema/ps.querySchema.js
@@ -0,0 +1,13 @@
+// 定义某个表的querySchema
+// schema的结构和含义参考下面的例子
+// 注意: 所有的key不能重复
+
+module.exports = [
+  {
+    key: 'productId',  // 传递给后端的字段名
+    title: 'productId',  // 前端显示的名称
+    placeholder: '请输入productId',  // 提示语, 可选
+    dataType: 'varchar',
+    showType: 'normal',
+  },
+];
